a871576c51fc63dbaefa734a4f132b0728345594
Carlos Louise Melchor <97199876+carloslmnait@users.noreply.github.com>
2024-02-08 21:32:22 -0700

    fourth commit - fix javascript and database

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+	import { resolve } from 'path';
+	import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": "./src",
+	// https://vitejs.dev/config/
+	export default defineConfig({
+		server: {
+			port: 3000,
 		},
-	},
-	build: {
-		rollupOptions: {
-			input: {
-				index: resolve(__dirname, "./index.html"),
-				demo: resolve(__dirname, "./live-demo.html"),
-				user: resolve(__dirname, "./user.html"),
-				login: resolve(__dirname, "./login.html"),
+		resolve: {
+			alias: {
+				'@': './src',
 			},
 		},
-	},
-});
+		build: {
+			rollupOptions: {
+				input: {
+					index: resolve(__dirname, './index.html'),
+					demo: resolve(__dirname, './live-demo.html'),
+					login: resolve(__dirname, './login.html'),
+				},
+			},
+		},
+	});
+
+/* 
+
+*/
