9bb4e7b318f4f092120ede53953b43100c1adcae
Carlos Louise Melchor <97199876+carloslmnait@users.noreply.github.com>
2024-02-08 23:18:51 -0700

    seventh commit - fix js target to only live-demo.html

--- js/index.js
+++ js/index.js
@@ -1,25 +1,5 @@
 import { getToDoData } from './lib/firebase/api';
 import { toDoItemTemplate } from './templates/toDoItem';
-/* 
-           1. Import Data 
-		   2. Working with Object of Objects 
-		      Object.values()
-			  Object.keys()
-			  Object.entries()
-
-		    3. Creating A Template Preline... Tailwind UI Libraries 
-
-			4. Creating A Render Function 
-
-			5 Updating the DOM.
-
-			Display Templating 
-			toDoItems.forEach((todo) => {
-		      document.querySelector('#root').insertAdjacentHTML('afterbegin', todo);
-	         });
-
-			  
-*/
 
 async function appInit() {
     const appData = await getToDoData();
@@ -33,7 +13,8 @@ async function appInit() {
         div.appendChild(markup);
     });
     // Corrected line: append the 'div' containing all toDoItems to 'main', not 'markup'
-    document.querySelector('main').append(div);
+    // only show at the main.live-demo 
+    document.querySelector('main.live-demo').append(div);
 }
 
 appInit();
