d9dea083d0b84864c8075db4d35491d80aaec13e
Carlos Louise Melchor <97199876+carloslmnait@users.noreply.github.com>
2024-02-08 21:51:47 -0700

    fifth commit - fix index.js

--- js/index.js
+++ js/index.js
@@ -22,23 +22,17 @@ import { toDoItemTemplate } from './templates/toDoItem';
 */
 
 async function appInit() {
-	const appData = await getToDoData();
-	// Interactive Templating
-	const toDoItems = Object.values(appData).map((obj) => {
-		return toDoItemTemplate(obj.todo);
-	});
+    const appData = await getToDoData();
+    const toDoItems = Object.values(appData).map((obj) => {
+        return toDoItemTemplate(obj.todo);
+    });
 
-	// render function jsx +++ data
-	// template fn() passing the data   =====> template string htmlish string
-
-	// interactive templating
-	// container
-	// batch update .... to dom updates all at once
-	const div = document.createElement('div');
-	toDoItems.forEach((markup) => {
-		div.appendChild(markup);
-	});
-	document.querySelector('main').append(markup);
+    const div = document.createElement('div');
+    toDoItems.forEach((markup) => {
+        div.appendChild(markup);
+    });
+    // Corrected line: append the 'div' containing all toDoItems to 'main', not 'markup'
+    document.querySelector('main').append(div);
 }
 
 appInit();
